90bffcef94601137225a4c7262dd6b7543704ca0
卢壮壮 <lzz@pcuu.com>
2019-02-27 14:06:22 +0800

    redux学习

--- src/App.js
+++ src/App.js
@@ -1,68 +1,89 @@
 import React from 'react'
-import {Button,List} from 'antd-mobile'
 
+// import {Button,List} from 'antd-mobile'
+// // 1、react组件的练习
+// class App extends React.Component{
+//   render(){
+//     const boss = '李云龙'
+//     return (
+//       <div>
+//         <h2>独立团，团长{boss}</h2>
+//         <一营 老大="张大彪"></一营>
+//         <骑兵连 老大="孙德胜">></骑兵连>
+//       </div>
+//     )
+    
+//   }
+// }
+
+// function 骑兵连(props){
+//   return <h2>骑兵连连长{props.老大},冲啊！</h2>
+// }
+
+// class 一营 extends React.Component{
+//   constructor(props){
+//     super(props)
+//     this.state = {
+//       solders:['虎子','柱子','王根生']
+//     }
+//     // bind this
+//     // this.addSolder = this.addSolder.bind(this)
+//   }
+//   componentWillMount(){
+//     console.log('组件马上就要加载了')
+//   }
+//   componentDidMount(){
+//     console.log('组件加载完毕')
+//   }
+//   addSolder(){
+//     console.log('hello add solder!')
+//     this.setState({
+//       solders:[...this.state.solders, '新兵蛋子' + Math.random()]
+//     })
+//   }
+//   render(){
+//     console.log('组件正在加载')
+//     return (
+//       <div>
+//         <h2>一营，营长{this.props.老大}</h2>
+//         <Button type="primary" onClick={()=> this.addSolder()}>新兵入伍</Button>
+//         <List renderHeader = {() => '士兵列表'} >
+//           {this.state.solders.map(v=>{
+//             return (
+//               <List.Item key={v}>
+//                 {v}
+//               </List.Item>
+//             )
+//           })}
+//         </List>
+
+//         {/* <ul>
+//           {this.state.solders.map(v =>{
+//             return <li key={v}>{v}</li>
+//           })}
+//         </ul> */}
+//       </div>
+//     )
+//   }
+// }
+
+// 2、redux的练习
 class App extends React.Component{
   render(){
-    const boss = '李云龙'
+    const store = this.props.store
+    const num = store.getState()
+    const addGun = this.props.addGun
+    const removeGun = this.props.removeGun
+    const addGunAsysc = this.props.addGunAsysc
     return (
       <div>
-        <h2>独立团，团长{boss}</h2>
-        <一营 老大="张大彪"></一营>
-        <骑兵连 老大="孙德胜">></骑兵连>
-      </div>
-    )
-    
-  }
-}
-
-function 骑兵连(props){
-  return <h2>骑兵连连长{props.老大},冲啊！</h2>
-}
-
-class 一营 extends React.Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      solders:['虎子','柱子','王根生']
-    }
-    // bind this
-    // this.addSolder = this.addSolder.bind(this)
-  }
-  componentWillMount(){
-    console.log('组件马上就要加载了')
-  }
-  componentDidMount(){
-    console.log('组件加载完毕')
-  }
-  addSolder(){
-    console.log('hello add solder!')
-    this.setState({
-      solders:[...this.state.solders, '新兵蛋子' + Math.random()]
-    })
-  }
-  render(){
-    console.log('组件正在加载')
-    return (
-      <div>
-        <h2>一营，营长{this.props.老大}</h2>
-        <Button type="primary" onClick={()=> this.addSolder()}>新兵入伍</Button>
-        <List renderHeader = {() => '士兵列表'} >
-          {this.state.solders.map(v=>{
-            return (
-              <List.Item key={v}>
-                {v}
-              </List.Item>
-            )
-          })}
-        </List>
-
-        {/* <ul>
-          {this.state.solders.map(v =>{
-            return <li key={v}>{v}</li>
-          })}
-        </ul> */}
+        <h1>现在有机枪{num}把</h1>
+        <button onClick={() => store.dispatch(addGun())}>申请武器</button>
+        <button onClick={() => store.dispatch(removeGun())}>上交武器</button>
+        <button onClick={() => store.dispatch(addGunAsysc())}>拖两天在给</button>
       </div>
     )
   }
 }
+
 export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
+// 1、正常模式
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore, applyMiddleware} from 'redux'
+import {counter, addGun, removeGun, addGunAsysc} from './index.redux'
+import thunk from 'redux-thunk';
+const store = createStore(counter, applyMiddleware(thunk))
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function render(){
+  ReactDOM.render(<App store={store} addGun={addGun} removeGun={removeGun} addGunAsysc={addGunAsysc}/>, document.getElementById('root'));
+}
+render()
+store.subscribe(render)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+// // 2、练习redux
+// import {createStore} from 'redux'
+// // 1、新建store
+// // 2、通过reducer建立
+// // 3、根据老state和action 生成新的state
+// function counter(state=0,action){
+//   switch(action.type){
+//     case '加机关枪':
+//       return state + 1;
+//     case '减机关枪':
+//       return state - 1;
+//     default:
+//     return 10
+//   }
+// }
+
+// // (1) 新建store
+// const store = createStore(counter)
+
+// const init = store.getState()
+// console.log(init)
+
+// function listener(){
+//   const current = store.getState()
+//   console.log(`现在有机关枪${current}把`)
+// }
+// store.subscribe(listener)
+// // 派发事件 传递action
+// store.dispatch({type:'加机关枪'})
+
